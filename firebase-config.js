3fd7133481d04ae62a99ba6f963523e32b0b4760
4simpleproblems <4simpleproblems@gmail.com>
2025-05-27 16:23:06 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,14 @@
 // firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// !!! IMPORTANT: Replace with your actual Firebase project configuration !!!
-// You can find this information in your Firebase project settings
-// (Project settings -> General -> Your apps -> Firebase SDK snippet -> Config)
-const firebaseConfig = {
-  apiKey: "AIzaSyBeqn-t-e-hZhYV_6zh1UCvrk38HhYOZ7o",
-  authDomain: "foursimpleproblems.firebaseapp.com",
-  projectId: "foursimpleproblems",
-  storageBucket: "foursimpleproblems.firebasestorage.app",
-  messagingSenderId: "329009885873",
-  appId: "1:329009885873:web:4b907162188effc788e890",
-  measurementId: "G-9CQPE80MXD"
-};
+// Replace with your real config from Firebase Console
+const firebaseConfig = { apiKey: "AIzaSyBeqn-t-e-hZhYV_6zh1UCvrk38HhYOZ7o", authDomain: "foursimpleproblems.firebaseapp.com", projectId: "foursimpleproblems", storageBucket: "foursimpleproblems.firebasestorage.app", messagingSenderId: "329009885873", appId: "1:329009885873:web:4b907162188effc788e890", measurementId: "G-9CQPE80MXD" };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Get a reference to the authentication service
-const auth = firebase.auth();
-
-// Get a reference to the Firestore database
-const db = firebase.firestore();
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
