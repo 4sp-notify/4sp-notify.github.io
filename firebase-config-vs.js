d6077dc6f25527a661f10e0aa550b1fc00606c4f
4SP <4simpleproblems@gmail.com>
2025-06-18 14:04:06 -0400

    Delete firebase-config-vs.js

--- firebase-config-vs.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBtn_Jse0f6PjXqUpN8wcPu_IbGQGH9LoM",
-  authDomain: "vern-social.firebaseapp.com",
-  projectId: "vern-social",
-  storageBucket: "vern-social.firebasestorage.app",
-  messagingSenderId: "822592699675",
-  appId: "1:822592699675:web:aa93d2d1420c9db94927be",
-  measurementId: "G-Y2X7042965"
-};
