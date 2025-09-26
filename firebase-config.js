317a32a654312d7454a8f38976636415c314f0f6
Attentio <102916166+Attentio-web@users.noreply.github.com>
2025-09-26 17:17:18 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbaYBA-Ab3noI-p2ZC57sR7OnLcc7ojL8",
-  authDomain: "foursimpleproblems-v4.firebaseapp.com",
-  projectId: "foursimpleproblems-v4",
-  storageBucket: "foursimpleproblems-v4.firebasestorage.app",
-  messagingSenderId: "251178815270",
-  appId: "1:251178815270:web:cc7fdb5ed05f8d8d203be7"
+  apiKey: "AIzaSyCEMOXNV5CNK6ds4rXyzB2hX9EB36ZDpNQ",
+  authDomain: "attentio-web.firebaseapp.com",
+  projectId: "attentio-web",
+  storageBucket: "attentio-web.firebasestorage.app",
+  messagingSenderId: "965345902625",
+  appId: "1:965345902625:web:b60bff09e35fc0e9c56360"
 };
 
 // Initialize Firebase
